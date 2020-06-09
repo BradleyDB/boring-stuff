f09748cef0c0820cae2cb6389eef5b3452571023
Bradley <bradley.daybazhaw@gmail.com>
2020-06-09 16:08:19 -0700

    Update url_email_phone_text_parser.py
    Cleaned up some of the regex code, changed the functions to list comprehensions, and cleaned up the output and some of the later variables.

--- url_email_phone_text_parser.py
+++ url_email_phone_text_parser.py
@@ -15,12 +15,11 @@ import pyperclip
 #Create a regular expression object for phone numbers
 phone_number_regex = re.compile(r'''
 #Phone number types 123-555-1234,123-5555,(123)-123-5555,(123) 123-1234,3333331234,123.123.1234,123-1234 ext 123, ext. 123, x1234           
-
-(((\d\d\d) | (\(\d\d\d\)))?    #area code (optional)
+(((\d{3}) | (\(\d{3\)))?       #area code (optional)
 (\.|\-|\s)                     #first separator
-\d\d\d                         #first 3 digits
+\d{3}                          #first 3 digits
 (\.|\-|\s)                     #second seperator
-\d\d\d\d                       #last 4 digits
+\d{4}                          #last 4 digits
 (((ext(\.)?\s)|x)              #extension word part (optional)
 (\d{2,5}))?)                   #extension number (optional)           
            
@@ -35,7 +34,6 @@ email_regex = re.compile(r'''
         [a-zA-Z0-9._-]+ # email domain
         \.              # period before .com etc
         [a-zA-Z0-9]{2,} # edu, com or other extension               
-
 ''',re.VERBOSE)
 
 #Create a regular expression object for websites
@@ -55,21 +53,14 @@ www\d{0,3}[.]|          #a www or host name 'mirror' domain;
 )''',re.VERBOSE)
 
 #Make a function that adds extracted text to a list    
-def make_list(extracted_list_variable_name):
-    new_empty_list = []
-    for thing in extracted_list_variable_name:
-        new_empty_list.append(thing[0])
-    return new_empty_list
 
+def make_list(extracted_list_variable_name):
+    return [thing[0] for thing in extracted_list_variable_name]
 
 #Make a function that counts the number of items returned from a list
-    
-def count_of_things(extracted_list_variable_name):
-    new_empty_list = []
-    for thing in extracted_list_variable_name:
-        new_empty_list.append(thing[0])
-    return str(len(new_empty_list))
 
+def count_of_things(extracted_list_variable_name):
+    return len([thing[0] for thing in extracted_list_variable_name])
 
 #Get the text off the clipboard
 
@@ -88,11 +79,21 @@ all_url = make_list(extracted_url)
 
 #Get the count of items in each list
 
-phone_number_count = count_of_things(extracted_phone)
-email_count = (str(len(extracted_email)))
-url_count = count_of_things(extracted_url)
+phone_number_count = len(all_phone)
+email_count = len(extracted_email)
+url_count = len(all_url)
 
 #Copy the extracted email/phone numbers to the cliboard
-    
-output = 'Found the Following ' + phone_number_count + ' Phone Numbers:' + '\n' + '\n'.join(all_phone) + '\n' + '\n' + 'Found the Following ' + email_count + ' Email Addresses:' + '\n' + '\n'.join(extracted_email) + '\n' + '\n' + 'Found the following ' + url_count + ' URLs:' + '\n' + '\n'.join(all_url)
+nl = '\n'    
+output = f'''
+Found the Following {phone_number_count} Phone Numbers:
+{nl.join(all_phone)}
+
+Found the Following {email_count} Email Addresses:
+{nl.join(extracted_email)}
+
+Found the following {url_count} URLs:
+{nl.join(all_url)}
+'''
+
 pyperclip.copy(output)
